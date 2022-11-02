90473bef30eb9e0d94685f383f419e46bf058075
Ilya Kolesnikov <82774653+godsended@users.noreply.github.com>
2022-11-02 18:32:42 +0300

    Delete private directory

--- private/payeer/payeerInfo.js
+++
@@ -1,7 +0,0 @@
-const payeerAccount = 'P1080107233';
-const payeerApiId = '1765801640';
-const payeerApiPass = 'lPRNkA31o2z3jUZI';
-
-exports.payeerAccount = payeerAccount;
-exports.payeerApiId = payeerApiId;
-exports.payeerApiPass = payeerApiPass;
--- private/postgres/pgConnection.js
+++
@@ -1,8 +0,0 @@
-const connection = {
-    user: 'ilakolesnikov',
-    host: 'localhost',
-    database: 'mgdb',
-    password: '174285396020',
-    port: 5432
-}
-exports.connection = connection;
